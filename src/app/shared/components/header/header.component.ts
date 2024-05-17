b0eb8838f2aefa1b0e8c195579d23d2712e6d800
rodrities <rodrities@hotmail.com>
2024-05-17 04:04:37 -0500

    fix bugs

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  username: any  = localStorage.getItem('user')
+export class HeaderComponent implements OnInit{
+  
+  username: string | null  = localStorage.getItem('user')
+  ngOnInit(): void {
+    if (this.username !== null)
+    this.username = this.username.split(' ')[0];
+  }
 }
